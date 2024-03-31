04e4f590923810c7b2bfe6a8d5f4d9a0a6869557
rebecayucratovar <andrearebecayucratovar@gmail.com>
2024-03-30 22:35:51 -0400

    Conexion a BD MySQL

--- servicios/src/main/java/org/edutech/servicioss/infraestructura/conexion/BDconexionImpl.java
+++ servicios/src/main/java/org/edutech/servicioss/infraestructura/conexion/BDconexionImpl.java
@@ -5,11 +5,11 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class BDconexionImpl implements BDconexion{
-  private static final String PORT = ""; // environment variables
-  private static final String USER = ""; // environment variables
-  private static final String PASSWORD = ""; // environment variables
-  private static final String HOST = ""; // 127.0.0.1 // environment variables
-  private static final String DB_NAME = ""; // environment variables
+  private static final String PORT = "3306"; // environment variables
+  private static final String USER = "uphlrap4jho52sxm"; // environment variables
+  private static final String PASSWORD = "H6Ract7DiaK09oeYjmOI"; // environment variables
+  private static final String HOST = "bpbrow7nzuvik4o584fx-mysql.services.clever-cloud.com"; // 127.0.0.1 // environment variables
+  private static final String DB_NAME = "bpbrow7nzuvik4o584fx"; // environment variables
 
   public Connection getConnection() throws SQLException {
     return DriverManager.getConnection(createDBURL(), USER, PASSWORD);
