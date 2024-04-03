c5fcc5a0cfd83993931f2f14919e7663b0d84fa7
ShirMari <shirleycm67@gmai.com>
2024-04-03 09:06:56 -0400

    modificacion de conexion a base de datos

--- servicios/src/main/java/org/edutech/servicioss/infraestructura/repositorios/CusoRepositorio.java
+++ servicios/src/main/java/org/edutech/servicioss/infraestructura/repositorios/CusoRepositorio.java
@@ -1,2 +1,14 @@
-package org.edutech.servicioss.infraestructura.repositorios;public interface CusoRepositorio {
+package org.edutech.servicioss.infraestructura.repositorios;
+
+import org.edutech.servicioss.infraestructura.tablas.Curso;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.UUID;
+
+@Repository
+public interface CusoRepositorio extends JpaRepository<Curso, UUID> {
+  List<Curso> findByName(String nombre);
+
 }
