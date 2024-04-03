c0afd257df9f9999619cdc6c5413d22030cc7435
ShirMari <shirleycm67@gmai.com>
2024-04-03 10:20:35 -0400

    Crear servicios y controlador de cursos

--- servicios/src/main/java/org/edutech/servicioss/controlador/CursoControlador.java
+++ servicios/src/main/java/org/edutech/servicioss/controlador/CursoControlador.java
@@ -1,4 +1,57 @@
 package org.edutech.servicioss.controlador;
 
+import lombok.RequiredArgsConstructor;
+import org.edutech.servicioss.infraestructura.tablas.Curso;
+import org.edutech.servicioss.servicios.CursoServicio;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/v1/formulario_cursos")
+@RequiredArgsConstructor
 public class CursoControlador {
+  private final CursoServicio cursoServicio;
+
+  @PostMapping
+  public ResponseEntity<Curso> saveCurso(@RequestBody Curso curso){
+    if(curso.getNombre()==null|| curso.getNombre().trim().isEmpty()){
+      return ResponseEntity.badRequest().build();
+    }
+    return ResponseEntity.ok(cursoServicio.save(curso));
+  }
+
+  @GetMapping
+  public ResponseEntity<List<Curso>> getAllCursos(){
+    var cursoResult = cursoServicio.getAll();
+    if (cursoResult.isEmpty()){
+      return ResponseEntity.noContent().build();
+    }
+    return ResponseEntity.ok(cursoResult);
+  }
+
+  @PostMapping("/{cursoId}")
+  public ResponseEntity<Curso> getOne(@PathVariable("cursoId") UUID cursoId){
+    var cursoFound = cursoServicio.findById(cursoId);
+
+    if(cursoFound.isEmpty()){
+      return ResponseEntity.notFound().build();
+
+    }
+    return ResponseEntity.ok(cursoFound.get());
+  }
+
+  @PutMapping("/{cursoId}")
+  public ResponseEntity<Curso> update(@PathVariable("cursoId") UUID cursoId, @RequestBody Curso cursoBody) {
+    cursoBody.setId(cursoId);
+    return ResponseEntity.ok(cursoServicio.update(cursoBody));
+  }
+
+  @DeleteMapping("/{cursoId}")
+  public ResponseEntity<Void> delete(@PathVariable("cursoId") UUID cursoId) {
+    cursoServicio.deleteById(cursoId);
+    return ResponseEntity.noContent().build();
+  }
 }
--- servicios/src/main/java/org/edutech/servicioss/servicios/CursoServicioImpl.java
+++ servicios/src/main/java/org/edutech/servicioss/servicios/CursoServicioImpl.java
@@ -1,4 +1,41 @@
 package org.edutech.servicioss.servicios;
 
-public class CursoServicioImpl {
+import lombok.RequiredArgsConstructor;
+import org.edutech.servicioss.infraestructura.repositorios.CursoRepositorio;
+import org.edutech.servicioss.infraestructura.tablas.Curso;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+@Service
+@RequiredArgsConstructor
+public class CursoServicioImpl implements CursoServicio{
+  private final CursoRepositorio cursoRepositorio;
+
+  @Override
+  public List<Curso> getAll() {
+    return cursoRepositorio.findAll();
+  }
+
+  @Override
+  public Optional<Curso> findById(UUID id) {
+    return cursoRepositorio.findById(id);
+  }
+
+  @Override
+  public Curso save(Curso curso) {
+    return cursoRepositorio.save(curso);
+  }
+
+  @Override
+  public Curso update(Curso curso) {
+    return cursoRepositorio.save(curso);
+  }
+
+  @Override
+  public void deleteById(UUID id) {
+    cursoRepositorio.deleteById(id);
+  }
 }
