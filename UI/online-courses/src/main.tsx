9f57011adfed21bb80310952bb7d0b77da7539d2
darkmasterUMSS <202103833@est.umss.edu>
2024-04-03 21:41:22 -0400

    form-register-course-maquetacion

---
+++ UI/online-courses/src/main.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import { FormRegisterCourse } from "./pages/FormRegisterCourse";
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <FormRegisterCourse />
+  </React.StrictMode>
+);
