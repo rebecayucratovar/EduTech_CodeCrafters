d97bdc8190512c4bf5572dd84a16a60642a95c9a
rebecayucratovar <andrearebecayucratovar@gmail.com>
2024-04-05 15:13:27 -0400

    intentando solucionar errores

---
+++ servicios/src/main/java/org/edutech/servicioss/infraestructura/tablas/Usuario.java
@@ -0,0 +1,25 @@
+package org.edutech.servicioss.infraestructura.tablas;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+import org.edutech.servicioss.infraestructura.enums.TipoUsuario;
+import java.util.UUID;
+
+@Data
+@Entity
+public class Usuario {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.AUTO)
+  private UUID id;
+  private String nombreCompleto;
+  private String nombreUsuario;
+  private String fechaNacimiento;
+  private TipoUsuario tipoUsuario;
+  private String correoElectronico;
+  private String contrasena;
+
+}
---
+++ servicios/src/main/java/org/edutech/servicioss/servicios/UsuarioServicio.java
@@ -0,0 +1,15 @@
+package org.edutech.servicioss.servicios;
+
+import org.edutech.servicioss.infraestructura.tablas.Usuario;
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+public interface UsuarioServicio {
+  List<Usuario> getAll();
+  Optional<Usuario> findById(UUID id);
+  Usuario save(Usuario usuario);
+  Usuario update(Usuario usuario);
+  void deleteById(UUID id);
+}
+
