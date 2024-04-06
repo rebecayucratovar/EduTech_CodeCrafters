7e894c472623f48e8d274d88ec250af695203329
ShirMari <shirleycm67@gmail.com>
2024-04-05 23:25:12 -0400

    Crear llave foranea Usuario-Curso

---
+++ servicios/src/main/java/org/edutech/servicioss/infraestructura/tablas/Usuario.java
@@ -0,0 +1,36 @@
+package org.edutech.servicioss.infraestructura.tablas;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import org.edutech.servicioss.infraestructura.enums.TipoUsuario;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.UUID;
+
+@Data
+@Entity
+public class Usuario {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.AUTO)
+  private UUID id;
+
+  private String nombreCompleto;
+
+  private String nombreUsuario;
+
+  private LocalDate fechaNacimiento;
+  //@Enumerated(EnumType.STRING)
+  private String tipoUsuario;
+
+  private String correoElectronico;
+
+  private String contrasenia;
+
+  @Transient
+  private String confirmarContrasenia;
+
+  @OneToMany(mappedBy = "usuario")
+  private List<Curso> cursos;
+}
