4dfacc345dff21f3b6aefa59c0318e53f9364986
rebecayucratovar <andrearebecayucratovar@gmail.com>
2024-04-06 15:30:41 -0400

    arreglado detalles y haciendo test para guardar en la BD

--- servicios/src/main/java/org/edutech/servicioss/ServiciosApplication.java
+++ servicios/src/main/java/org/edutech/servicioss/ServiciosApplication.java
@@ -2,12 +2,17 @@ package org.edutech.servicioss;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class ServiciosApplication {
+public class ServiciosApplication extends SpringBootServletInitializer {
 
   public static void main(String[] args) {
     SpringApplication.run(ServiciosApplication.class, args);
   }
-
+  @Override
+  protected SpringApplicationBuilder configure(SpringApplicationBuilder application){
+    return application.sources(ServiciosApplication.class);
+  }
 }
