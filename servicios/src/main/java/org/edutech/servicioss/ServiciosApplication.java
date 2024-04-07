37990ce0211dae44726441031ba7ca92d4527d45
rebecayucratovar <andrearebecayucratovar@gmail.com>
2024-04-07 09:32:06 -0400

    probando conexion

--- servicios/src/main/java/org/edutech/servicioss/ServiciosApplication.java
+++ servicios/src/main/java/org/edutech/servicioss/ServiciosApplication.java
@@ -2,17 +2,11 @@ package org.edutech.servicioss;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class ServiciosApplication extends SpringBootServletInitializer {
+public class ServiciosApplication{
 
   public static void main(String[] args) {
     SpringApplication.run(ServiciosApplication.class, args);
-  }
-  @Override
-  protected SpringApplicationBuilder configure(SpringApplicationBuilder application){
-    return application.sources(ServiciosApplication.class);
   }
 }
