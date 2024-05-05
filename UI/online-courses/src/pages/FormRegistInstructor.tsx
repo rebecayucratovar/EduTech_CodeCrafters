a4c242382bba43b4c155c0e4898ec1cc350d52f1
JoseMiguelRodriguez <josemiguelrodriguezpenia@gmail.com>
2024-05-04 22:46:51 -0400

    Solucion bug 9 redireccion a home

--- UI/online-courses/src/pages/FormRegistInstructor.tsx
+++ UI/online-courses/src/pages/FormRegistInstructor.tsx
@@ -534,6 +534,7 @@ export const FormRegistInstructor = () => {
                     onAccept={() => {
                         reset();
                         setShowModalError(false);
+                        navigate("/");
                     }}
                 />
             )}
@@ -548,7 +549,7 @@ export const FormRegistInstructor = () => {
                     onAccept={() => {
                         reset();
                         setShowModalByClickInCancel(false);
-                        navigate("/lista-cursos"); // Usa navigate en lugar de history.push
+                        navigate("/"); // Usa navigate en lugar de history.push
                     }}
                     onCancel={() => setShowModalByClickInCancel(false)}
                 />
