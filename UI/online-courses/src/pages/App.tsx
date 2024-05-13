2893e8ee38ef9a9ac19ea256ed1d8a8e7a397f6a
JoseMiguelRodriguez <josemiguelrodriguezpenia@gmail.com>
2024-05-12 22:41:32 -0400

    resolucion de conflicto

--- UI/online-courses/src/pages/App.tsx
+++ UI/online-courses/src/pages/App.tsx
@@ -11,14 +11,10 @@ export const App = () => {
     <div className="app-content">
       <Routes>
         <Route path="/" element={<Layout />}>
-<<<<<<< HEAD
           <Route
             path="registro-instructor"
             element={<FormRegistInstructor />}
           ></Route>
-=======
-          <Route path="registro-instructor" element={<FormRegistInstructor />}></Route>
->>>>>>> 5056be93c2a8da927a75f0eb039c8cdb7f618812
           <Route path="registro-curso" element={<FormRegisterCourse />}></Route>
           <Route path="lista-cursos" element={<ListCourses />}></Route>
             <Route path="comprar-cursos" element={<Comprar />} /> {/* Ruta para el formulario comprar.tsx */}
