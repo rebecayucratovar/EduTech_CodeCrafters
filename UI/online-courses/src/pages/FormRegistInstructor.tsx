067ebe8f0f234626f0857ebc1b31ebb894967447
rebecayucratovar <andrearebecayucratovar@gmail.com>
2024-05-19 15:26:21 -0400

    nuevo url deploy backend

--- UI/online-courses/src/pages/FormRegistInstructor.tsx
+++ UI/online-courses/src/pages/FormRegistInstructor.tsx
@@ -40,7 +40,7 @@ export const FormRegistInstructor = () => {
         // https://edutech-codecrafters-blue-water-8441.fly.dev/
         try {
             const response = await fetch(
-                `https://edutech-codecrafters-winter-sun-4756.fly.dev/v1/usuarios/verificar-correo?correo=${correo}`
+                `https://edutech-codecrafters-dry-mountain-212.fly.dev/v1/usuarios/verificar-correo?correo=${correo}`
             );
             const data = await response.json();
             return data.correoValido; // Devuelve true si el correo es único, false si no lo es
@@ -52,7 +52,7 @@ export const FormRegistInstructor = () => {
     const validateUsuario = async (nommbreUsuario: String) => {
         try {
             const response = await fetch(
-                `https://edutech-codecrafters-blue-water-8441.fly.dev/v1/usuarios/verificar-usuario?nombreUsuario=${nommbreUsuario}`
+                `https://edutech-codecrafters-dry-mountain-212.fly.dev/v1/usuarios/verificar-usuario?nombreUsuario=${nommbreUsuario}`
             );
             const data = await response.json();
             return data.usuarioValido; // Devuelve true si el correo es único, false si no lo es
@@ -64,7 +64,7 @@ export const FormRegistInstructor = () => {
 
     const onSubmit = handleSubmit(async (formData) => {
         try {
-            const response = await fetch("https://edutech-codecrafters-blue-water-8441.fly.dev/v1/usuarios/saveUsuario", {
+            const response = await fetch("https://edutech-codecrafters-dry-mountain-212.fly.dev/v1/usuarios/saveUsuario", {
                 method: "POST",
                 headers: {
                     "Content-Type": "application/json"
