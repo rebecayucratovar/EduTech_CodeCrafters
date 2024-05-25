fa47b53feaa3e95670b65d5e4905eaf8d5f80b74
rebecayucratovar <andrearebecayucratovar@gmail.com>
2024-05-25 18:14:10 -0400

    muestra nombre de instructor en "mis cursos"

--- servicios/src/main/java/org/edutech/servicioss/infraestructura/repositorios/CompraRepositorio.java
+++ servicios/src/main/java/org/edutech/servicioss/infraestructura/repositorios/CompraRepositorio.java
@@ -1,6 +1,7 @@
 package org.edutech.servicioss.infraestructura.repositorios;
 
 import org.edutech.servicioss.infraestructura.tablas.Compra;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
