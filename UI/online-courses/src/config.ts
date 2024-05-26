f1969002331e695b70792b1f9318617bf3c2b425
rebecayucratovar <andrearebecayucratovar@gmail.com>
2024-05-25 23:42:03 -0400

    nuevo url backend deployado con funcionalidad cantidad de estudiantes

--- UI/online-courses/src/config.ts
+++ UI/online-courses/src/config.ts
@@ -1,4 +1,4 @@
 // config.ts
-export const API_BASE_URL = 'http://localhost:3039/v1';
+export const API_BASE_URL = 'https://edutech-codecrafters-restless-grass-6607.fly.dev/v1';
 //https://edutech-codecrafters-blue-fire-8896.fly.dev
 // http://localhost:3039
