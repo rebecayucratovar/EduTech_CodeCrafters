dda5535ddf217953e44b4698b6abbd091424677b
JoseMiguelRodriguez <josemiguelrodriguezpenia@gmail.com>
2024-05-26 10:12:24 -0400

    salto de inicio de sesion al intentar agregar al carrito o comprar curso sin loguearse

--- UI/online-courses/src/components/ModalCursoDetail.tsx
+++ UI/online-courses/src/components/ModalCursoDetail.tsx
@@ -4,37 +4,46 @@ import Picture from "../assets/icons/Picture.svg";
 import Reset from "../assets/icons/Reset.svg";
 import Close from "../assets/icons/Close.svg";
 import { useCarro } from "../context/CarroProvider";
-import  {useNavigate}  from "react-router-dom";
-import { useState,useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import { useState, useEffect } from "react";
 import { Modal } from "./Modal";
+import ModalInicioSesion from "./ModalInicioSesion"; // Importar el componente ModalInicioSesion
 import { API_BASE_URL } from "../config.ts";
+
 export const ModalCursoDetail = ({ onClose, course }: any) => {
   const navigate = useNavigate();
   const { agregarAlCarrito, cursoEnCarrito } = useCarro();
-  const [isopen, setIsopen] = useState(false);
+  const [isOpen, setIsOpen] = useState(false);
   const [cursoComprado, setCursoComprado] = useState(false);
+  const [isLoginModalOpen, setIsLoginModalOpen] = useState(false);
+  const [tipoUsuario, setTipoUsuario] = useState<string | null>(null);
+  const [usuarioId, setUsuarioId] = useState<string | null>(null);
   const formattedCategoria = course.categoria.replace(/_/g, " ");
+  
   const handleCancel = () => {
     onClose();
   };
 
-  //const useCarro = () => useContext(CarroContexto);
   useEffect(() => {
-    const usuarioId = localStorage.getItem("usuarioId");
-    if (usuarioId) {
-      obtenerCursoComprado(usuarioId);
+    const tipo = localStorage.getItem("tipoUsuario");
+    const id = localStorage.getItem("usuarioId");
+    setTipoUsuario(tipo);
+    setUsuarioId(id);
+    
+    if (id) {
+      obtenerCursoComprado(id);
     }
   }, [course]);
 
   const obtenerCursoComprado = async (usuarioId: string) => {
     try {
       const response = await fetch(
-          `${API_BASE_URL}/compras/usuario/${usuarioId}`
+        `${API_BASE_URL}/compras/usuario/${usuarioId}`
       );
       if (response.ok) {
         const cursosComprados = await response.json();
         const cursoYaComprado = cursosComprados.some(
-            (cursoComprado: any) => cursoComprado.id === course.id
+          (cursoComprado: any) => cursoComprado.id === course.id
         );
         setCursoComprado(cursoYaComprado);
       } else {
@@ -43,21 +52,41 @@ export const ModalCursoDetail = ({ onClose, course }: any) => {
     } catch (error) {
       console.error("Error al obtener los cursos comprados:", error);
     }
-  };const handleComprarAhora = () => {
-    navigate("/comprar-cursos", { state: { cursos: [course] } });
+  };
+
+  const isUserLoggedIn = () => {
+    return !!localStorage.getItem("usuarioId");
+  };
+
+  const handleComprarAhora = () => {
+    if (isUserLoggedIn()) {
+      navigate("/comprar-cursos", { state: { cursos: [course] } });
+    } else {
+      setIsLoginModalOpen(true);
+    }
   };
 
   const handleAgregarAlCarrito = () => {
-    agregarAlCarrito(course);
-    setIsopen(true);
+    if (isUserLoggedIn()) {
+      agregarAlCarrito(course);
+      setIsOpen(true);
+    } else {
+      setIsLoginModalOpen(true);
+    }
   };
 
   const handleCursoYaComprado = () => {
     navigate("/mis-cursos");
   };
+
   const handleCursoEnCarrito = () => {
     navigate("/lista-compras");
   };
+
+  const handleLoginModalClose = () => {
+    setIsLoginModalOpen(false);
+  };
+
   return (
     <>
       {course && (
@@ -136,14 +165,17 @@ export const ModalCursoDetail = ({ onClose, course }: any) => {
                   </label>
 
                   <label
-                      htmlFor="instructor"
-                      className="modal-curso-detail-content-panel-right-text-first-instructor"
+                    htmlFor="instructor"
+                    className="modal-curso-detail-content-panel-right-text-first-instructor"
                   >
                     Instructor: {course.nombreCompletoUsuario}
-                  </label> <label htmlFor="extra"
-                                  className="modal-curso-detail-content-panel-right-text-first-instructor"
-
-                >Información extra:</label>
+                  </label>
+                  <label
+                    htmlFor="extra"
+                    className="modal-curso-detail-content-panel-right-text-first-instructor"
+                  >
+                    Información extra:
+                  </label>
                 </div>
 
                 <div className="modal-curso-detail-content-panel-right-text-second">
@@ -160,26 +192,48 @@ export const ModalCursoDetail = ({ onClose, course }: any) => {
                   <div className="modal-curso-detail-content-panel-right-text-second-item">
                     <img src={Reset} alt="icon" />
                     <label htmlFor="duracion">
-                      Ultima actualización: 08/04/2024
+                      Última actualización: 08/04/2024
                     </label>
                   </div>
 
                   <div className="modal-curso-detail-content-panel-right-text-second-item">
                     <img src={Book} alt="icon" />
-                    <label htmlFor="duracion">Estudiantes: {course.numeroEstudiantes}</label>
+                    <label htmlFor="duracion">
+                      Estudiantes: {course.numeroEstudiantes}
+                    </label>
                   </div>
                 </div>
 
                 <div className="modal-curso-detail-content-panel-right-buttons">
                   <button
-                      onClick={cursoComprado ? handleCursoYaComprado : cursoEnCarrito(course.id) ? handleCursoEnCarrito : handleComprarAhora}
+                    onClick={
+                      cursoComprado
+                        ? handleCursoYaComprado
+                        : cursoEnCarrito(course.id)
+                        ? handleCursoEnCarrito
+                        : handleComprarAhora
+                    }
                   >
-                    {cursoComprado ? "Curso ya comprado" : cursoEnCarrito(course.id) ? "Curso en el carrito" : "Comprar Ahora"}
+                    {cursoComprado
+                      ? "Curso ya comprado"
+                      : cursoEnCarrito(course.id)
+                      ? "Curso en el carrito"
+                      : "Comprar Ahora"}
                   </button>
                   <button
-                      onClick={cursoComprado ? handleCursoYaComprado : cursoEnCarrito(course.id) ? handleCursoEnCarrito : handleAgregarAlCarrito}
+                    onClick={
+                      cursoComprado
+                        ? handleCursoYaComprado
+                        : cursoEnCarrito(course.id)
+                        ? handleCursoEnCarrito
+                        : handleAgregarAlCarrito
+                    }
                   >
-                    {cursoComprado ? "Curso ya comprado" : cursoEnCarrito(course.id) ? "Curso en el carrito" : "Añadir al carrito"}
+                    {cursoComprado
+                      ? "Curso ya comprado"
+                      : cursoEnCarrito(course.id)
+                      ? "Curso en el carrito"
+                      : "Añadir al carrito"}
                   </button>
                 </div>
               </div>
@@ -188,19 +242,21 @@ export const ModalCursoDetail = ({ onClose, course }: any) => {
         </article>
       )}
 
-      {isopen && (
+      {isOpen && (
         <Modal
-        title=""
-        description="Se agregó a carrito"
-        txtBtnAccept="Aceptar"
-        onAccept={() => {
-          navigate("lista-compras")
-          setIsopen(false);
-        }}
+          title=""
+          description="Se agregó a carrito"
+          txtBtnAccept="Aceptar"
+          onAccept={() => {
+            navigate("lista-compras");
+            setIsOpen(false);
+          }}
+        />
+      )}
 
-    />
-)}
-
+      {isLoginModalOpen && (
+        <ModalInicioSesion onClose={handleLoginModalClose} /> // Mostrar el modal de inicio de sesión
+      )}
     </>
   );
 };
