04ab98f12fa9f4b3aa3cb520d6c8059c2e9ecfe0
rebecayucratovar <andrearebecayucratovar@gmail.com>
2024-05-27 08:40:45 -0400

    no permite comprar multiples veces un curso (desabilita botones)

--- UI/online-courses/src/components/ModalCursoDetail.tsx
+++ UI/online-courses/src/components/ModalCursoDetail.tsx
@@ -20,7 +20,7 @@ export const ModalCursoDetail = ({ onClose, course }: any) => {
   //const [usuarioId, setUsuarioId] = useState<string | null>(null);
   const [isLoading, setIsLoading] = useState(true); // Nuevo estado de carga
   const formattedCategoria = course.categoria.replace(/_/g, " ");
-  
+
   const handleCancel = () => {
     onClose();
   };
@@ -30,7 +30,7 @@ export const ModalCursoDetail = ({ onClose, course }: any) => {
     const id = localStorage.getItem("usuarioId");
     //setTipoUsuario(tipo);
     //setUsuarioId(id);
-    
+
     if (id) {
       obtenerCursoComprado(id);
     }
@@ -39,12 +39,12 @@ export const ModalCursoDetail = ({ onClose, course }: any) => {
   const obtenerCursoComprado = async (usuarioId: string) => {
     try {
       const response = await fetch(
-        `${API_BASE_URL}/compras/usuario/${usuarioId}`
+          `${API_BASE_URL}/compras/usuario/${usuarioId}`
       );
       if (response.ok) {
         const cursosComprados = await response.json();
         const cursoYaComprado = cursosComprados.some(
-          (cursoComprado: any) => cursoComprado.id === course.id
+            (cursoComprado: any) => cursoComprado.id === course.id
         );
         setCursoComprado(cursoYaComprado);
       } else {
@@ -91,177 +91,180 @@ export const ModalCursoDetail = ({ onClose, course }: any) => {
   };
 
   return (
-    <>
-      {course && (
-        <article className="modal-curso-detail">
-          <div className="modal-curso-detail-container">
-            <button className="modal-curso-btn-close" onClick={handleCancel}>
-              <img src={Close} alt="image" />
-            </button>
+      <>
+        {course && (
+            <article className="modal-curso-detail">
+              <div className="modal-curso-detail-container">
+                <button className="modal-curso-btn-close" onClick={handleCancel}>
+                  <img src={Close} alt="image" />
+                </button>
 
-            <section className="modal-curso-detail-content">
-              <div className="modal-curso-detail-content-panel-left">
-                <label
-                  htmlFor="title"
-                  className="modal-curso-detail-content-panel-left-title"
-                >
-                  {course.titulo}
-                </label>
-
-                <label
-                  htmlFor="subtitle"
-                  className="modal-curso-detail-content-panel-left-subtitle"
-                >
-                  Categoría: {formattedCategoria}
-                </label>
-
-                <div className="modal-curso-detail-content-panel-left-description">
-                  <label
-                    htmlFor="descripcion-title"
-                    className="modal-curso-detail-content-panel-left-description-title"
-                  >
-                    Descripción:
-                  </label>
-                  <p className="modal-curso-detail-content-panel-left-description-text">
-                    {course.descripcion}
-                  </p>
-                </div>
-
-                <div className="modal-curso-detail-content-panel-left-list">
-                  <label
-                    htmlFor="instructor"
-                    className="modal-curso-detail-content-panel-left-list-title"
-                  >
-                    Lo que aprenderás:
-                  </label>
-                  <ul className="modal-curso-detail-content-panel-left-list-items">
-                    <li>{course.aprenderas}</li>
-                  </ul>
-                </div>
-
-                <div className="modal-curso-detail-content-panel-left-list">
-                  <label
-                    htmlFor="instructor"
-                    className="modal-curso-detail-content-panel-left-list-title"
-                  >
-                    Requisitos del curso:
-                  </label>
-                  <ul className="modal-curso-detail-content-panel-left-list-items">
-                    <li>{course.requisitos}</li>
-                  </ul>
-                </div>
-              </div>
-
-              <div className="modal-curso-detail-content-panel-right">
-                <div className="modal-curso-detail-content-panel-right-img">
-                  <div className="modal-curso-detail-content-panel-right-img-container">
-                    <img src={course.imagen} alt="image" />
-                  </div>
-                </div>
-
-                <div className="modal-curso-detail-content-panel-right-text-first">
-                  <label
-                    htmlFor="costo"
-                    className="modal-curso-detail-content-panel-right-text-first-costo"
-                  >
-                    Costo: {course.costo} bs
-                  </label>
-
-                  <label
-                    htmlFor="instructor"
-                    className="modal-curso-detail-content-panel-right-text-first-instructor"
-                  >
-                    Instructor: {course.nombreCompletoUsuario}
-                  </label>
-                  <label
-                    htmlFor="extra"
-                    className="modal-curso-detail-content-panel-right-text-first-instructor"
-                  >
-                    Información extra:
-                  </label>
-                </div>
-
-                <div className="modal-curso-detail-content-panel-right-text-second">
-                  <div className="modal-curso-detail-content-panel-right-text-second-item">
-                    <img src={Timer} alt="icon" />
-                    <label htmlFor="duracion">Duración: 5 semanas</label>
-                  </div>
-
-                  <div className="modal-curso-detail-content-panel-right-text-second-item">
-                    <img src={Picture} alt="icon" />
-                    <label htmlFor="duracion">Nivel: Intermedio</label>
-                  </div>
-
-                  <div className="modal-curso-detail-content-panel-right-text-second-item">
-                    <img src={Reset} alt="icon" />
-                    <label htmlFor="duracion">
-                      Última actualización: 08/04/2024
+                <section className="modal-curso-detail-content">
+                  <div className="modal-curso-detail-content-panel-left">
+                    <label
+                        htmlFor="title"
+                        className="modal-curso-detail-content-panel-left-title"
+                    >
+                      {course.titulo}
                     </label>
-                  </div>
 
-                  <div className="modal-curso-detail-content-panel-right-text-second-item">
-                    <img src={Book} alt="icon" />
-                    <label htmlFor="duracion">
-                      Estudiantes: {course.numeroEstudiantes}
+                    <label
+                        htmlFor="subtitle"
+                        className="modal-curso-detail-content-panel-left-subtitle"
+                    >
+                      Categoría: {formattedCategoria}
                     </label>
+
+                    <div className="modal-curso-detail-content-panel-left-description">
+                      <label
+                          htmlFor="descripcion-title"
+                          className="modal-curso-detail-content-panel-left-description-title"
+                      >
+                        Descripción:
+                      </label>
+                      <p className="modal-curso-detail-content-panel-left-description-text">
+                        {course.descripcion}
+                      </p>
+                    </div>
+
+                    <div className="modal-curso-detail-content-panel-left-list">
+                      <label
+                          htmlFor="instructor"
+                          className="modal-curso-detail-content-panel-left-list-title"
+                      >
+                        Lo que aprenderás:
+                      </label>
+                      <ul className="modal-curso-detail-content-panel-left-list-items">
+                        <li>{course.aprenderas}</li>
+                      </ul>
+                    </div>
+
+                    <div className="modal-curso-detail-content-panel-left-list">
+                      <label
+                          htmlFor="instructor"
+                          className="modal-curso-detail-content-panel-left-list-title"
+                      >
+                        Requisitos del curso:
+                      </label>
+                      <ul className="modal-curso-detail-content-panel-left-list-items">
+                        <li>{course.requisitos}</li>
+                      </ul>
+                    </div>
                   </div>
-                </div>
 
-                {!isLoading && (
-                <div className="modal-curso-detail-content-panel-right-buttons">
-                  <button
-                    onClick={
-                      cursoComprado
-                        ? handleCursoYaComprado
-                        : cursoEnCarrito(course.id)
-                        ? handleCursoEnCarrito
-                        : handleComprarAhora
-                    }
-                  >
-                    {cursoComprado
-                      ? "Curso ya comprado"
-                      : cursoEnCarrito(course.id)
-                      ? "Curso en el carrito"
-                      : "Comprar Ahora"}
-                  </button>
-                  <button
-                    onClick={
-                      cursoComprado
-                        ? handleCursoYaComprado
-                        : cursoEnCarrito(course.id)
-                        ? handleCursoEnCarrito
-                        : handleAgregarAlCarrito
-                    }
-                  >
-                    {cursoComprado
-                      ? "Curso ya comprado"
-                      : cursoEnCarrito(course.id)
-                      ? "Curso en el carrito"
-                      : "Añadir al carrito"}
-                  </button>
-                </div>
-                    )}
+                  <div className="modal-curso-detail-content-panel-right">
+                    <div className="modal-curso-detail-content-panel-right-img">
+                      <div className="modal-curso-detail-content-panel-right-img-container">
+                        <img src={course.imagen} alt="image" />
+                      </div>
+                    </div>
+
+                    <div className="modal-curso-detail-content-panel-right-text-first">
+                      <label
+                          htmlFor="costo"
+                          className="modal-curso-detail-content-panel-right-text-first-costo"
+                      >
+                        Costo: {course.costo} bs
+                      </label>
+
+                      <label
+                          htmlFor="instructor"
+                          className="modal-curso-detail-content-panel-right-text-first-instructor"
+                      >
+                        Instructor: {course.nombreCompletoUsuario}
+                      </label>
+                      <label
+                          htmlFor="extra"
+                          className="modal-curso-detail-content-panel-right-text-first-instructor"
+                      >
+                        Información extra:
+                      </label>
+                    </div>
+
+                    <div className="modal-curso-detail-content-panel-right-text-second">
+                      <div className="modal-curso-detail-content-panel-right-text-second-item">
+                        <img src={Timer} alt="icon" />
+                        <label htmlFor="duracion">Duración: 5 semanas</label>
+                      </div>
+
+                      <div className="modal-curso-detail-content-panel-right-text-second-item">
+                        <img src={Picture} alt="icon" />
+                        <label htmlFor="duracion">Nivel: Intermedio</label>
+                      </div>
+
+                      <div className="modal-curso-detail-content-panel-right-text-second-item">
+                        <img src={Reset} alt="icon" />
+                        <label htmlFor="duracion">
+                          Última actualización: 08/04/2024
+                        </label>
+                      </div>
+
+                      <div className="modal-curso-detail-content-panel-right-text-second-item">
+                        <img src={Book} alt="icon" />
+                        <label htmlFor="duracion">
+                          Estudiantes: {course.numeroEstudiantes}
+                        </label>
+                      </div>
+                    </div>
+
+                    <div className="modal-curso-detail-content-panel-right-buttons">
+                      <button
+                          onClick={
+                            cursoComprado
+                                ? handleCursoYaComprado
+                                : cursoEnCarrito(course.id)
+                                    ? handleCursoEnCarrito
+                                    : handleComprarAhora
+                          }
+                          disabled={isLoading} // Deshabilitar el botón mientras se carga
+                          className={isLoading ? "button-disabled" : ""}
+
+                      >
+                        {cursoComprado
+                            ? "Curso ya comprado"
+                            : cursoEnCarrito(course.id)
+                                ? "Curso en el carrito"
+                                : "Comprar Ahora"}
+                      </button>
+                      <button
+                          onClick={
+                            cursoComprado
+                                ? handleCursoYaComprado
+                                : cursoEnCarrito(course.id)
+                                    ? handleCursoEnCarrito
+                                    : handleAgregarAlCarrito
+                          }
+                          disabled={isLoading} // Deshabilitar el botón mientras se carga
+                          className={isLoading ? "button-disabled" : ""}
+                      >
+                        {cursoComprado
+                            ? "Curso ya comprado"
+                            : cursoEnCarrito(course.id)
+                                ? "Curso en el carrito"
+                                : "Añadir al carrito"}
+                      </button>
+                    </div>
+                  </div>
+                </section>
               </div>
-            </section>
-          </div>
-        </article>
-      )}
+            </article>
+        )}
 
-      {isOpen && (
-        <Modal
-          title=""
-          description="Se agregó a carrito"
-          txtBtnAccept="Aceptar"
-          onAccept={() => {
-            navigate("lista-compras");
-            setIsOpen(false);
-          }}
-        />
-      )}
+        {isOpen && (
+            <Modal
+                title=""
+                description="Se agregó a carrito"
+                txtBtnAccept="Aceptar"
+                onAccept={() => {
+                  navigate("lista-compras");
+                  setIsOpen(false);
+                }}
+            />
+        )}
 
-      {isLoginModalOpen && (
-        <ModalInicioSesion onClose={handleLoginModalClose} /> // Mostrar el modal de inicio de sesión
-      )}
-    </>
+        {isLoginModalOpen && (
+            <ModalInicioSesion onClose={handleLoginModalClose} /> // Mostrar el modal de inicio de sesión
+        )}
+      </>
   );
 };
